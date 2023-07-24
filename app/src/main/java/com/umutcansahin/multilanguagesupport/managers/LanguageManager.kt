211bfff9bd342e9bd8aa063ccccdfc0b76b87eb0
Umutcan Sahin <umutcan199726@gmail.com>
2023-07-24 13:14:46 +0300

    get current language code without context

--- app/src/main/java/com/umutcansahin/multilanguagesupport/managers/LanguageManager.kt
+++ app/src/main/java/com/umutcansahin/multilanguagesupport/managers/LanguageManager.kt
@@ -1,18 +1,13 @@
 package com.umutcansahin.multilanguagesupport.managers
 
-import android.content.Context
-import android.content.ContextWrapper
-import android.content.res.Configuration
 import androidx.appcompat.app.AppCompatDelegate
 import androidx.core.os.LocaleListCompat
-import dagger.hilt.android.qualifiers.ApplicationContext
-import java.util.Locale
 import javax.inject.Inject
 import javax.inject.Singleton
 
 @Singleton
 class LanguageManager @Inject constructor(
-    @ApplicationContext private val context: Context
+//    @ApplicationContext private val context: Context
 ) {
 
     fun takeLanguageCode(languageCode: String) {
@@ -24,18 +19,40 @@ class LanguageManager @Inject constructor(
     }
 
     private fun setLanguage(languageCode: String) {
+        AppCompatDelegate.setApplicationLocales(LocaleListCompat.forLanguageTags(languageCode))
+    }
+
+    fun getCurrentLanguage(): String {
+        // return context.resources.configuration.locales.get(0).language
+        return AppCompatDelegate.getApplicationLocales().get(0)?.language ?: "en"
+    }
+}
+//interface AppLanguageProvider{
+//    fun getAppLanguage(): String
+//    fun setAppLanguage(languageCode: String?)
+//}
+//
+//
+//class AppLanguageProviderImpl @Inject constructor(
+//    private val context: Context
+//) : AppLanguageProvider {
+//
+//    override fun getAppLanguage(): String {
+//        val configuration = context.resources.configuration
+//        val locale = Locale.forLanguageTag(configuration.locale.language)
+//        return when (locale) {
+//            Locale("tr") -> "tr"
+//            Locale("en") -> "en"
+//            else -> "tr"
+//        }
+//    }
+//
+//    override fun setAppLanguage(languageCode: String?) {
 //        val locale = Locale(languageCode)
 //        Locale.setDefault(locale)
 //        val resources = context.resources
 //        val config: Configuration = resources.configuration
 //        config.setLocale(locale)
 //        resources.updateConfiguration(config, resources.displayMetrics)
-//
-
-        AppCompatDelegate.setApplicationLocales(LocaleListCompat.forLanguageTags(languageCode))
-    }
-
-    fun getCurrentLanguage(): String {
-        return context.resources.configuration.locales.get(0).language
-    }
-}
+//    }
+//}
